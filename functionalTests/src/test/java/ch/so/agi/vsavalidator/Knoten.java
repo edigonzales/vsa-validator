cd2020c734d61316966b1fd97c3b3d110c387301
Stefan Ziegler <edi.gonzales@gmail.com>
2021-03-27 15:33:14 +0100

    add Cid_2040

--- functionalTests/src/test/java/ch/so/agi/vsavalidator/Knoten.java
+++ functionalTests/src/test/java/ch/so/agi/vsavalidator/Knoten.java
@@ -99,4 +99,21 @@ public class Knoten {
         String content = new String(Files.readAllBytes(Paths.get(logFileName)));
         assertTrue(content.contains("Error: line 20: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: Nicht als solches attributiertes Sonderbauwerk"));
     }
+    
+    @Test
+    public void Cid_2040(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/2040/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/2040/config.toml");
+        settings.setValue(Validator.SETTING_PLUGINFOLDER, "../lib/build/libs");
+
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/2040/2040.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertTrue(content.contains("Warning: line 37: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001002: Kein Ueberlauf_Foerderaggregat erfasst"));        
+    }
 }
