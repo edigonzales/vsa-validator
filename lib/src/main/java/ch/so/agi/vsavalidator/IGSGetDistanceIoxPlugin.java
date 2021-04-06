25c0169e644833ae62368faf84d7b2202cef8f11
Stefan Ziegler <edi.gonzales@gmail.com>
2021-04-06 20:38:53 +0200

    start with IGS_getDistance

---
+++ lib/src/main/java/ch/so/agi/vsavalidator/IGSGetDistanceIoxPlugin.java
@@ -0,0 +1,64 @@
+package ch.so.agi.vsavalidator;
+
+import ch.ehi.basics.settings.Settings;
+import ch.interlis.ili2c.metamodel.TransferDescription;
+import ch.interlis.iom.IomObject;
+import ch.interlis.iox.IoxValidationConfig;
+import ch.interlis.iox_j.logging.LogEventFactory;
+import ch.interlis.iox_j.validator.InterlisFunction;
+import ch.interlis.iox_j.validator.ObjectPool;
+import ch.interlis.iox_j.validator.Value;
+
+public class IGSGetDistanceIoxPlugin implements InterlisFunction {
+    private LogEventFactory logger = null;
+
+    @Override
+    public Value evaluate(String validationKind, String usageScope, IomObject mainObj, Value[] actualArguments) {
+        if (actualArguments[0].skipEvaluation()) {
+            return actualArguments[0];
+        }
+        if (actualArguments[0].isUndefined()) {
+            return Value.createSkipEvaluation();
+        }
+        if (actualArguments[1].isUndefined()) {
+            return Value.createSkipEvaluation();
+        }
+        
+        System.out.println("---------------");
+        System.out.println(actualArguments[0].getComplexObjects());
+        
+        // IGS_getFirstPoint: Vergleichen mit Knote_nachRef.
+        
+        
+//        Double value1;
+//        Double value2;
+//        if (actualArguments[0].getValue() != null) {
+//            value1 = Double.parseDouble(actualArguments[0].getValue());
+//        } else {
+//            value1 = Double.valueOf(actualArguments[0].getNumeric());
+//        }
+//        
+//        if (actualArguments[1].getValue() != null) {
+//            value2 = Double.parseDouble(actualArguments[1].getValue());
+//        } else {
+//            value2 = Double.valueOf(actualArguments[1].getNumeric());
+//        }
+//        
+//        return new Value(value1 + value2);
+        return Value.createSkipEvaluation();
+    }
+
+    @Override
+    public String getQualifiedIliName() {
+        return "IGSFunction.IGS_getDistance";
+    }
+
+    @Override
+    public void init(TransferDescription td, Settings settings, 
+            IoxValidationConfig validationConfig, ObjectPool objectPool, 
+            LogEventFactory logEventFactory) {
+
+        this.logger = logEventFactory;
+        this.logger.setValidationConfig(validationConfig);
+    }
+}
